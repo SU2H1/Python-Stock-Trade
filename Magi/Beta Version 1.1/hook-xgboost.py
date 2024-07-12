25e88918cf6c00a095747b52fc326155fd942f24
SU2H1 <87033231+SU2H1@users.noreply.github.com>
2024-07-12 07:09:47 +0000

    Update hook-xgboost.py

--- Magi/Beta Version 1.1/hook-xgboost.py
+++ Magi/Beta Version 1.1/hook-xgboost.py
@@ -1,3 +1,3 @@
-from PyInstaller.utils.hooks import collect_all
-
-datas, binaries, hiddenimports = collect_all('xgboost')
+xgboost_path = os.path.dirname(xgboost.__file__)
+xgboost_lib = os.path.join(xgboost_path, 'lib', 'libxgboost.dylib')
+datas += [(xgboost_lib, 'xgboost/lib')]
